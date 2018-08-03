590c0c2115d8df67ec7c3ddac92333457769fc36
David Helkowski <dhelkowski@suse.com>
2018-08-03 14:04:54 -0700

    Dex OIDC connector support; ui, models, db

--- db/schema.rb
+++ db/schema.rb
@@ -194,4 +194,40 @@ ActiveRecord::Schema.define(version: 20181715075511) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "dex_connectors_ldap", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name",               limit: 255
+    t.string   "host",               limit: 255
+    t.integer  "port",               limit: 2,   default: 636
+    t.boolean  "start_tls",                      default: false,                                       null: false
+    t.boolean  "bind_anon",                      default: false,                                       null: false
+    t.string   "bind_dn",            limit: 255, default: "uid=someuid,cn=users,dc=somedomain,dc=com"
+    t.string   "bind_pw",            limit: 255
+    t.string   "username_prompt",    limit: 255, default: "Username"
+    t.string   "user_base_dn",       limit: 255, default: "cn=users,dc=somedomain,dc=com"
+    t.string   "user_filter",        limit: 255, default: "(objectClass=person)"
+    t.string   "user_attr_username", limit: 255, default: "uid"
+    t.string   "user_attr_id",       limit: 255, default: "uid"
+    t.string   "user_attr_email",    limit: 255, default: "mail",                                      null: false
+    t.string   "user_attr_name",     limit: 255, default: "name"
+    t.string   "group_base_dn",      limit: 255, default: "cn=groups,dc=somedomain,dc=com"
+    t.string   "group_filter",       limit: 255, default: "(objectClass=group)"
+    t.string   "group_attr_user",    limit: 255, default: "uid"
+    t.string   "group_attr_group",   limit: 255, default: "member"
+    t.string   "group_attr_name",    limit: 255, default: "name"
+  end
+
+  create_table "dex_connectors_oidc", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name",               limit: 255
+    t.string   "provider_url",       limit: 255
+    t.string   "client_id",          limit: 255
+    t.string   "client_secret",      limit: 255
+    t.string   "callback_url",       limit: 255, default: "http://127.0.0.1:5556/callback"
+    t.boolean  "basic_auth",                     default: true
+  end
+
+  add_index "dex_connectors_ldap", ["id"], name: "index_dex_connectors_ldap_on_id", unique: true, using: :btree
 end
