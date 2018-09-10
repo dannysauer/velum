6d8ca2e31f2db21cd3b5aa0496089e17ad48f100
Danny Sauer <dsauer@suse.com>
2018-09-10 10:46:22 -0500

    Fix and complete test cases

--- app/models/concerns/oidc_provider_validator.rb
+++ app/models/concerns/oidc_provider_validator.rb
@@ -1,9 +1,7 @@
 # Validate that column contains a reachable OIDC Provider
-class OidcProviderValidator < ActiveModel::EachValidator
-  # TODO: take record as a parameter, and add appropriate error on validation steps
-  #       ...or create specific exceptions that can be caught?
-  # currently, just returns true/false, so all failures result in the same message
+class OidcProviderValidator < HttpUrlValidator
   def self.compliant?(value)
+    return false unless super
     parsed_uri = URI.parse(value)
     unless parsed_uri.is_a?(URI::HTTPS)
       # SWD will be replaced with Webfinger in OIDC gem eventually.
@@ -16,17 +14,21 @@ class OidcProviderValidator < ActiveModel::EachValidator
     ## validate method compares issuer to expected issuer
     response.validate(value)
     # TODO: also check supported methods?
-  rescue SocketError
-    false # hostname not resolvable
-  rescue OpenIDConnect::Discovery::DiscoveryFailed
-    false # any error with webfinger / issuer mismatch
-  rescue URI::InvalidURIError
-    false # malformed URL
   end
 
   def validate_each(record, attribute, value)
+    return false unless super
     return true if value.present? && self.class.compliant?(value)
     record.errors.add(attribute, "is not a valid OIDC provider")
     false
+  rescue SocketError
+    record.errors.add(attribute, "is not a valid OIDC provider: bad/unresolvable hostname")
+    false # hostname not resolvable
+  rescue OpenIDConnect::Discovery::DiscoveryFailed
+    record.errors.add(attribute, "is not a valid OIDC provider: discovery failure")
+    false # any error with webfinger / issuer mismatch
+  rescue HTTPClient::ConnectTimeoutError
+    record.errors.add(attribute, "is not a valid OIDC provider: connection timeout")
+    false # The System Is Down / StongBad Techno
   end
 end
--- app/models/dex_connector_oidc.rb
+++ app/models/dex_connector_oidc.rb
@@ -2,17 +2,10 @@
 class DexConnectorOidc < ActiveRecord::Base
   self.table_name = "dex_connectors_oidc"
 
-  # should :name have a "uniqueness: true" validation instead?
   validates :name,          presence: true
-  # format: { with: /\Asome regex\z/, message: "must match regex" }
-  # Maybe /https?:\/\/(user)?(:pass)?@?host(.domain)*(:port)?(\/.*)/
-  #
-  # Or maybe a validates_with custom validator which attempts the connection?
-  # https://guides.rubyonrails.org/active_record_validations.html#validates-with
-  # https://stackoverflow.com/a/7167988/65589
-  validates :provider_url,  http_url: true, oidc_provider: true
+  validates :provider_url,  presence: true, http_url: true, oidc_provider: true
   validates :client_id,     presence: true
   validates :client_secret, presence: true
-  validates :callback_url,  http_url: true
+  validates :callback_url,  presence: true, http_url: true
   validates :basic_auth,    presence: true
 end
