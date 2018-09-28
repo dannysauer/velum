8e02aa8c47ffa56370dacfe02c06f33d8024dc0a
Danny Sauer <dsauer@suse.com>
2018-09-28 16:44:38 -0500

    Fix duplicated 'non-http URL' error message

--- app/models/dex_connector_oidc.rb
+++ app/models/dex_connector_oidc.rb
@@ -3,7 +3,7 @@ class DexConnectorOidc < ActiveRecord::Base
   self.table_name = "dex_connectors_oidc"
 
   validates :name,          presence: true
-  validates :provider_url,  presence: true, http_url: true, oidc_provider: true
+  validates :provider_url,  presence: true, oidc_provider: true
   validates :client_id,     presence: true
   validates :client_secret, presence: true
   validates :callback_url,  presence: true, http_url: true
