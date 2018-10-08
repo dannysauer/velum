4f7811b2ec091ce1cfc65e9e284d022f74c3cf82
Danny Sauer <dsauer@suse.com>
2018-10-08 16:52:06 -0500

    Formatting
    Move comment to module for Rubocop
    Switch to more Ruby-ish JSON format

--- lib/velum/dex/oidc.rb
+++ lib/velum/dex/oidc.rb
@@ -1,21 +1,20 @@
 require "base64"
 
-# This generates the JSON that SaltStack uses to list the OIDC connectors
 module Velum
+  # This generates the JSON that SaltStack uses to list the OIDC connectors
   module Dex
     class << self
       def oidc_connectors_as_pillar
         DexConnectorOidc.all.map do |con|
           {
-            "type"          => "oidc",
-            "id"            => "oidc-" + con.id.to_s,
-            "name"          => con.name,
-
-            "provider_url"  => con.provider_url,
-            "client_id"     => con.client_id,
-            "client_secret" => con.client_secret,
-            "callback_url"  => con.callback_url,
-            "basic_auth"    => con.basic_auth
+            type:          "oidc",
+            id:            "oidc-" + con.id.to_s,
+            name:          con.name,
+            provider_url:  con.provider_url,
+            client_id:     con.client_id,
+            client_secret: con.client_secret,
+            callback_url:  con.callback_url,
+            basic_auth:    con.basic_auth
           }
         end
       end
