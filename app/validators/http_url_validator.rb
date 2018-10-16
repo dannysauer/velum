98a94944a3d8ec04a37d9cacb83913cad34aface
Danny Sauer <dsauer@suse.com>
2018-10-16 09:40:26 -0500

    note for future developer

--- app/validators/http_url_validator.rb
+++ app/validators/http_url_validator.rb
@@ -1,6 +1,8 @@
 # Validate that a URL is a legal http/https format URL
 class HttpUrlValidator < ActiveModel::EachValidator
   def self.compliant?(value)
+    # Ruby URI complains if any underscores are in the FQDN
+    # value = value.gsub('_','')
     uri = URI.parse(value)
     # https inherits from http, so both "is a" http
     uri.is_a?(URI::HTTP) && !uri.host.nil?
